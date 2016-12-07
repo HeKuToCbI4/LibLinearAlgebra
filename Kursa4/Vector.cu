2518c636429d7d38ee8fb6d3d424cf108e4c5aa3
HeKuToCbI4 <leonov2424@mail.ru>
2016-12-07 09:41:16 +0400

    memfis

--- Kursa4/Vector.cu
+++ Kursa4/Vector.cu
@@ -1,72 +1,174 @@
-#include "Vector.cuh"
+
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
+#include <iostream>
+#include <vector>
+#include <exception>
+using namespace std;
 
 template <class T>
-__global__ void addKernel(T *c, const T *a, const T *b, const unsigned int &N)
+__global__ void addKernel(T *c, const T *a, const T *b, size_t N)
 {
-	unsigned int i = threadIdx.x + blockIdx.x*blockDim.x;
-	if (i<N)
-		c[i] = a[i] + b[i];
+	size_t i = threadIdx.x*16 + blockIdx.x*blockDim.x*16;
+	for (size_t j(0); j < 16; j++)
+		if (i + j < N)
+			c[i + j] = a[i + j] + b[i + j];
 }
 template <class T>
-__global__ void mulKernel(const T* a, const T *b, T *c, const unsigned int &N)
+__global__ void mulKernel(const T* a, const T* b, T *c, size_t N)
 {
-	unsigned int i = threadIdx.x + blockIdx.x*blockDim.x;
-	if (i<N)
-		c[i] = a[i] * b[i];
+	size_t i = threadIdx.x*16 + blockIdx.x*blockDim.x*16;
+	for (size_t j(0); j < 16; j++)
+		if (i + j < N)
+		c[i+j] = a[i+j] * b[i+j];
 }
+
 template <class T, class X>
-__global__ void mulByNum(const T*a, const X n, T* b, const unsigned int &N)
+__global__ void mulByNum(const T*a, T* b, const X n, size_t N)
 {
-	unsigned int i = threadIdx.x + blockIdx.x*blockDim.x;
-	if (i < N)
-		b[i] = a[i] * n;
+	size_t i = threadIdx.x*16 + blockIdx.x*blockDim.x*16;
+	for (char j = 0; j < 16; j++)
+		if (i + j < N)
+			b[i + j] = a[i + j] * n;
+}
+template <class T>
+__global__ void sumVec(const T* a, double* num, size_t size)
+{
+	num[0] = 0;
+	for (size_t i(0); i < size; i++)
+		num[0] += a[i];
 }
 
-template <class T>
-Vector<T>::Vector()
-{
-}
+
+
 
 template <class T>
-Vector<T>::~Vector()
+class Vector : public vector<T>
 {
-}
+public:
+	Vector<T>() : vector<T>()
+	{
+	}
+	Vector<T>(size_t size) : vector<T>(size)
+	{
+	}
+	Vector operator +(const Vector<T>& a)
+	{
+		Vector<T> b = *this;
+		Vector<T> result = Vector(a.size());
+		T* d_a;
+		T* d_b;
+		T* d_c;
+		T* h_c;
+		if (a.size() != b.size())
+			throw exception("YOBA");
+		size_t size = sizeof(T)*a.size();
+		h_c = (T*)malloc(size);
+		if (cudaMalloc(&d_a, size) != cudaSuccess)
+		{
+			cout << "error in memory allocation\n";
+			getchar();
+		}
+		cudaMalloc(&d_b, size);
+		cudaMalloc(&d_c, size);
+		if (cudaMemcpy(d_a, &a[0], size, cudaMemcpyHostToDevice) != cudaSuccess)
+		{
+			cout << "error in memory copy from host to device\n";
+			getchar();
+		}
+		cudaMemcpy(d_b, &b[0], size, cudaMemcpyHostToDevice);
+		size_t threadsperblock = 16;
+		size_t blockspergrid = (a.size() + threadsperblock * 16 - 1) / threadsperblock / 16;
+		addKernel << <blockspergrid, threadsperblock >> > (d_c, d_a, d_b, a.size());
+		if (cudaSuccess != cudaGetLastError())
+		{
+			cout << "Error in kernel!\n";
+			getchar();
+		}
+		if (cudaMemcpy(h_c, d_c, size, cudaMemcpyDeviceToHost) != cudaSuccess)
+		{
+			cout << "error in copying memory from device to host\n";
+			getchar();
+		}
+		result.assign(h_c, h_c + a.size());
+		cudaFree(d_a);
+		cudaFree(d_b);
+		cudaFree(d_c);
+		free(h_c);
+		return result;
+	}
+	friend double operator *(const Vector<T>& a, const Vector<T> &b)
+	{
+		T* d_a;
+		T* d_b;
+		T* d_c;
+		if (a.size() != b.size())
+			throw exception("YOBA");
+		size_t size = sizeof(T)*a.size();
+		if (cudaMalloc(&d_a, size) != cudaSuccess)
+		{
+			cout << "error in memory allocation\n";
+			getchar();
+		}
+		cudaMalloc(&d_b, size);
+		cudaMalloc(&d_c, size);
+		if (cudaMemcpy(d_a, &a[0], size, cudaMemcpyHostToDevice) != cudaSuccess)
+		{
+			cout << "error in memory copy from host to device\n";
+			getchar();
+		}
+		cudaMemcpy(d_b, &b[0], size, cudaMemcpyHostToDevice);
+		size_t threadsperblock = 16;
+		size_t blockspergrid = (a.size() + threadsperblock * 16 - 1) / threadsperblock / 16;
+		mulKernel <<<blockspergrid, threadsperblock >>> (d_a, d_b, d_c, a.size());
+		if (cudaSuccess != cudaGetLastError())
+		{
+			cout << "Error in kernel!\n";
+			getchar();
+		}
+		double* sum;
+		sum = (double*)malloc(sizeof(double));
+		double* d_sum;
+		cudaMalloc(&d_sum, sizeof(double));
+		sumVec<<<1,1>>>(d_c, d_sum, a.size());
+		cudaMemcpy(sum, d_sum, sizeof(double), cudaMemcpyDeviceToHost);
+		cudaFree(d_a);
+		cudaFree(d_b);
+		cudaFree(d_c);
+		return sum[0];
+	}
+	template <class X>
+	friend Vector operator *(const Vector<T>& a, const X& b)
+	{
+		Vector<T> result = Vector(a.size());
+		T* d_a;
+		T* d_b;
+		T* h_c;
+		size_t size = sizeof(T)*a.size();
+		h_c = (T*)malloc( size);
+		cudaMalloc(&d_a, size);
+		cudaMalloc(&d_b, size);
+		cudaMemcpy(d_a, &a[0], size, cudaMemcpyHostToDevice);
+		size_t threadsperblock = 16;
+		size_t blockspergrid = (a.size() + threadsperblock * 16 - 1) / threadsperblock / 16;
+		mulByNum << <blockspergrid, threadsperblock >> > (d_a, d_b, b, a.size());
+		cudaMemcpy(h_c, d_b, size, cudaMemcpyDeviceToHost);
+		result.assign(h_c, h_c + a.size());
+		cudaFree(d_a);
+		cudaFree(d_b);
+		free(h_c);
+		return result;
+	}
+	template <class X>
+	friend Vector operator *(const X& b, const Vector<T>& a)
+	{
+		return a*b;
+	}
+	Vector cross_multiple(const Vector<T>&);
+	Vector mixed_multiple(const Vector<T>&);
+};
 
-template <class T>
-Vector<T> Vector<T>::operator+(const Vector<T>& a, const Vector<T>& b)
-{
-	Vector<T> result;
-	T* d_a;
-	T* d_b;
-	T* d_c;
-	T* h_c;
-	if (a.length() != b.length)
-		return nullptr;
-	unsigned int size = sizeof(T)*a.length();
-	malloc(&h_c, size);
-	cudaMalloc(&d_a, size);
-	cudaMalloc(&d_b, size);
-	cudaMalloc(&d_c, size);
-	cudaMemcpy(d_a, &a[0], size, cudaMemcpyHostToDevice); 
-	cudaMemcpy(d_b, &b[0], size, cudaMemcpyHostToDevice);
-	unsigned int threadsperblock = 256;
-	unsigned int blockspergrid = (a.length() + threadsperblock - 1) / threadsperblock;
-	addKernel << <blockspergrid, threadsperblock >> > (d_a, d_b, d_c, size);
-	cudaMemcpy(h_c, d_c, size, cudaMemcpyDeviceToHost);
-	result.assign(h_c, h_c + a.length());
-	cudaFree(d_a);
-	cudaFree(d_b);
-	cudaFree(d_c);
-	free(h_c);
-	return result;
-}
 
-template <class T>
-float Vector<T>::operator*(const Vector<T>&, const Vector<T>&)
-{
-}
 
 template <class T>
 Vector<T> Vector<T>::cross_multiple(const Vector<T>&)
@@ -78,34 +180,33 @@ Vector<T> Vector<T>::mixed_multiple(const Vector<T>&)
 {
 }
 
-template <class T, class X>
-Vector<T> operator *(const Vector<T>& a, const X& b)
+template <class T>
+void print_vec(Vector<T> vec)
 {
-	Vector<T> result;
-	T* d_a;
-	T* d_b;
-	T* h_c;
-	T d_x;
-	cudaMalloc(&d_x, sizeof(T));
-	cudaMemCpy(d_x, b, sizeof(T), cudaMemcpyHostToDevice);
-	unsigned int size = sizeof(T)*a.length();
-	malloc(&h_c, size);
-	cudaMalloc(&d_a, size);
-	cudaMalloc(&d_b, size);
-	cudaMemcpy(d_a, &a[0], size, cudaMemcpyHostToDevice);
-	unsigned int threadsperblock = 256;
-	unsigned int blockspergrid = (a.length() + threadsperblock - 1) / threadsperblock;
-	mulByNum<<<blockspergrid, threadsperblock >>> (d_a, d_x, size);
-	cudaMemcpy(h_c, d_b, size, cudaMemcpyDeviceToHost);
-	result.assign(h_c, h_c + a.length());
-	cudaFree(d_a);
-	cudaFree(d_b);
-	cudaFree(d_x);
-	free(h_c);
-	return result;
+	for (auto e : vec)
+		cout << e << endl;
+	getchar();
 }
 
-/*cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
+int main()
+{
+	Vector<double> vec1, vec2, vec3;
+	for (int i = 0; i < 15; i++)
+	{
+		vec1.emplace_back(1.1*i);
+		vec2.emplace_back(1.124*i*i);
+	}
+	//int* mem = (int*)malloc(vec1.size()*sizeof(int));
+	//mem = &vec1[0];
+	vec3 = vec1 + vec1;
+	double scalar_multiple = vec1*vec2;
+	print_vec(vec3);
+	cout << "SCALAR: " << scalar_multiple << endl;
+	getchar();
+	return 0;
+}
+
+/*cudaError_t addWithCuda(int *c, const int *a, const int *b, size_t size)
 {
 	int *dev_a = 0;
 	int *dev_b = 0;
